2f816392162493f83fa85d7aa28f757d061ed20c
edrhat <eduardomchortolandia@gmail.com>
2021-10-19 14:33:27 -0300

    Alterei a interface mas tem a mesma funcionalidade

--- api-cep.py
+++ api-cep.py
@@ -4,21 +4,30 @@ import tkinter as tk
 class Tela:
 
     def __init__(self, master):
-        
-        self.cep=tk.Entry(janela)
+
+        self.cep = tk.Label(janela, text="Informe o CEP:")
+        self.cep["font"] = ("Arial", "17")
         self.cep.place(x=5, y=10)
+        
+        self.cepE=tk.Entry(janela)
+        self.cepE["font"] = ("Arial","16")
+        self.cepE.place(x=175, y=10, width="100", height="30")
 
         self.bt1=tk.Button(janela, text="Consultar")
-        self.bt1.place(x=145, y=10)
+        self.bt1.place(x=295, y=4)
+        self.bt1["font"] = ("Arial", "14")
+        self.bt1.config(foreground="white", bg="green")
         self.bt1.bind("<Button-1>", self.ir)
 
     def ir(self, event):
 
-        cepE = self.cep.get()
-        print(cepE)
-        if len(cepE) != 8:
+        cepp = self.cepE.get()
+        print(cepp)
+        if len(cepp) != 8:
+            
             exit()
-        requisicao = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep_input))
+            
+        requisicao = requests.get('https://viacep.com.br/ws/{}/json/'.format(cepp))
         print(requisicao.json())
 
         
@@ -27,7 +36,7 @@ class Tela:
 janela = tk.Tk()
 Tela(janela)
 
-janela.geometry("300x200")
+janela.geometry("400x200")
 janela.mainloop()
 
 #print('-------------------')
